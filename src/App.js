bd11711fe70592b35222c2514f0078f9568dfb88
ting-hu <huxu@uwplatt.edu>
2023-02-21 21:19:30 -0600

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
 import Home from "./page/Home";
 import MovieDetails from "./page/MovieDetails/MovieDetails";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/Movie-Search/">
         <div className="app">
-          <Routes>
+          <Switch>
             <Route path="/" element={<Home />} />
             <Route path={`/movies/:id`} element={<MovieDetails />} />
-          </Routes>
+          </Switch>
         </div>
       </BrowserRouter>
     </div>
